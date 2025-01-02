efa500d3103e77f22e507714f4677e97bc2c1455
Jason Blocklove <jason.blocklove@gmail.com>
2025-01-01 22:39:53 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	description = "Flake for installing khal from Git and enabling via a module";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs";
+	};
+
+	outputs = { self, nixpkgs, ... }:
+		let
+		system = builtins.currentSystem;
+	pkgs = import nixpkgs { inherit system; };
+	in
+	{
+		packages.${system}.khal = pkgs.python3Packages.buildPythonApplication {
+			pname = "khal-git";
+			version = "git";
+			src = pkgs.fetchFromGitHub {
+				owner = "pimutils";
+				repo = "khal";
+				rev = "master";
+			};
+			propagatedBuildInputs = with pkgs.python3Packages; [
+				urwid
+				click
+				pytz
+				requests
+			];
+		};
+
+		nixosModules.khal = import ./modules/khal.nix;
+
+	};
+}
+
